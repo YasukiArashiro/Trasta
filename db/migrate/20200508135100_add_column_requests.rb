d1b9203dfc5ce1a0e567782b9057e3384dff264b
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-05-11 18:48:04 +0900

    [Fix]マイグレーションファイル修正

--- db/migrate/20200508135100_add_column_requests.rb
+++ db/migrate/20200508135100_add_column_requests.rb
@@ -1,5 +1,5 @@
 class AddColumnRequests < ActiveRecord::Migration[5.2]
   def up
-  	add_column :requests, :opponent_user_id, :intger
+  	add_column :requests, :opponent_user_id, :integer
   end
 end
