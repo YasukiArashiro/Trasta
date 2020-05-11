b702eef9e9a5e48192cd4ab342b39b46a743c238
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-05-11 17:48:39 +0900

    [fix]マイグレーションファイル修正

--- db/migrate/20200509012844_add_column_evaluation.rb
+++ db/migrate/20200509012844_add_column_evaluation.rb
@@ -1,11 +1,11 @@
 class AddColumnEvaluation < ActiveRecord::Migration[5.2]
   def up
-  	add_column :evaluations, :request_id, :intger
-  	change_column :evaluations, :evaluation, :intger
+  	add_column :evaluations, :request_id, :integer
+  	change_column :evaluations, :evaluation, :integer
   end
 
   def down
-  	remove_column :evaluations, :request_id, :intger
+  	remove_column :evaluations, :request_id, :integer
   	change_column :evaluations, :evaluation, :string
   end
 end
--- db/migrate/20200509122218_add_column_evaluations.rb
+++ db/migrate/20200509122218_add_column_evaluations.rb
@@ -1,11 +1,11 @@
 class AddColumnEvaluations < ActiveRecord::Migration[5.2]
   def up
-  	add_column :evaluations, :evaluation_partner, :intger
+  	add_column :evaluations, :evaluation_partner, :integer
   	remove_column :evaluations, :evaluation_status, :boolean
   end
 
   def down
   	add_column :evaluations, :evaluation_status, :boolean
-  	remove_column :evaluations, :evaluation_partner, :intger
+  	remove_column :evaluations, :evaluation_partner, :integer
   end
 end
