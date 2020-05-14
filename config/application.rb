329c961b469575ab781ba1d52ec1abd9b7e4ddb6
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-05-14 11:31:47 +0000

    [fix]タイムゾーンの修正

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Trasta
 
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
