696fef0e07ef03d4218e1a7dc30e5ada2ec04055
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-05-23 15:24:49 +0000

    [Fix]メール通知機能の設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # ----------------追記---------------------
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV['MAILER_EMAIL'],
+    password:  ENV['MAILER_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  # ----------------追記---------------------
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
